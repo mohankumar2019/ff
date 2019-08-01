b8c96923c694711c9a12bff423f229aa4954b837
mohan <mohan.chanti4@gmail.com>
2019-08-01 15:27:57 +0530

    new changes

---
+++ src/main/java/org/com/app/mvnff/App.java
@@ -0,0 +1,13 @@
+package org.com.app.mvnff;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "mvnff" );
+    }
+}
